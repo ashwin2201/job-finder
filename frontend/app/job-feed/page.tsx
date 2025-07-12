ab29b1483dc50d62239772772b500342e3457de5
ashwin2201 <ashwin2201@yahoo.com>
2025-07-12 15:57:15 +0900

    moved components

--- frontend/app/job-feed/page.tsx
+++ frontend/app/job-feed/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
-import JobPosting from '../components/JobPosting';
+import JobPosting from '../../components/JobPosting';
 import { Job } from "@/types/Job";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
